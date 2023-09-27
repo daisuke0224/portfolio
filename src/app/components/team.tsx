478f4a7ac20a2207eef9c37b30317060cb22ea7f
染井大輔 <someidaisuke@someidaisukenoMacBook-Pro.local>
2023-09-27 14:12:23 +0900

    teamIdが同一のユーザーのみ表示するコンポーネント作成

---
+++ src/app/components/team.tsx
@@ -0,0 +1,48 @@
+//@ts-nocheck
+"use client";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "@/firebase/client";
+import { useEffect, useState } from "react";
+
+// ドキュメントを取得する関数を作成
+const getDocumentsByTeamId = async (venderTeamId: string) => {
+  const q = query(
+    collection(db, "customers"),
+    where("venderTeamId", "==", venderTeamId)
+  );
+  const snapshot = await getDocs(q);
+  const documents = snapshot.docs.map((doc) => doc.data());
+  return documents;
+};
+
+// コンポーネント内で取得したドキュメントを表示する例
+const ExampleComponent = ({ venderTeamId }: { venderTeamId: string }) => {
+  const [documents, setDocuments] = useState([]);
+
+  useEffect(() => {
+    const fetchDocuments = async () => {
+      const result = await getDocumentsByTeamId(venderTeamId);
+      setDocuments(result);
+    };
+
+    fetchDocuments();
+  }, [venderTeamId]);
+  console.log(documents);
+
+  return (
+    <div>
+      {documents.map((document) => (
+        <div key={document.id}>
+          {/* ドキュメントの情報を表示するコンポーネントを表示 */}
+          <p>{document.customerName}</p>
+          <p>{document.piece}</p>
+          <p>{document.income}</p>
+
+          {/* ... */}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ExampleComponent;
